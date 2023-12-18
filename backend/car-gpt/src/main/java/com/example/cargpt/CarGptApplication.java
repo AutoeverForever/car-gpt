5c45f356c6a6927a1a7b94a4fabb2c0cf9ee7ba3
Jealyang <jaelyangchoi@gmail.com>
2023-12-18 23:03:02 +0900

    포트 추가

--- backend/car-gpt/src/main/java/com/example/cargpt/CarGptApplication.java
+++ backend/car-gpt/src/main/java/com/example/cargpt/CarGptApplication.java
@@ -17,7 +17,7 @@ public class CarGptApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:80");
 			}
 		};
 	}
