6c1ebe35cc1ed3997b269aa9fe5700b5d18e235e
Jealyang <jaelyangchoi@gmail.com>
2023-12-19 00:22:26 +0900

    운영 서버 ip로 변경

--- backend/car-gpt/src/main/java/com/example/cargpt/CarGptApplication.java
+++ backend/car-gpt/src/main/java/com/example/cargpt/CarGptApplication.java
@@ -17,7 +17,7 @@ public class CarGptApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:80");
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
